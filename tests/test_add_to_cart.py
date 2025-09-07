18474161ee556852617dfa2b7fc9db609c858b4a
annakraevskaya <kraevskayaannaa@mail.ru>
2025-09-07 16:53:34 +0300

    1

--- tests/test_add_to_cart.py
+++ tests/test_add_to_cart.py
@@ -1,19 +1,15 @@
-import time
-
-from pages.login_page import LoginPage
-from base.base_pages import BasePage
-from base.base_test import BaseTest
-
-
-class AddToCartProducts(BaseTest):
-
-    def test_add_to_cart_product(self):
-        self.login_page.open()
-        self.login_page.enter_login("standard_user")
-        self.login_page.enter_password("secret_sauce")
-        self.login_page.login_button()
-        time.sleep(2)
-
-        self.products_page.click_add_to_cart_backpack()
-        self.products_page.click_add_to_cart_fleece_jacket()
-        time.sleep(2)
+# import time
+# import pytest
+#
+# from pages.account_pages.products_page import ProductsPage
+# from base.base_test import BaseTest
+#
+#
+# class AddToCartProducts(BaseTest):
+#
+#     def test_add_to_cart_product(self):
+#         self.products_page.open()
+#
+#         self.products_page.click_add_to_cart_backpack()
+#         self.products_page.click_add_to_cart_fleece_jacket()
+#         time.sleep(2)
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,17 +1,17 @@
-# import time
-#
-# from pages.login_page import LoginPage
-# from base.base_pages import BasePage
-# from base.base_test import BaseTest
-#
-#
-# class TestLogin(BaseTest):
-#     def test_login(self):
-#         self.login_page.open()
-#         self.login_page.enter_login("standard_user")
-#         self.login_page.enter_password("secret_sauce")
-#         self.login_page.login_button()
-#         time.sleep(2)
-#         self.products_page.click_add_to_cart_backpack()
-#         self.products_page.click_add_to_cart_fleece_jacket()
-#         time.sleep(2)
+import time
+
+from pages.login_page import LoginPage
+from base.base_pages import BasePage
+from base.base_test import BaseTest
+
+
+class TestLogin(BaseTest):
+    def test_login(self):
+        self.login_page.open()
+        self.login_page.enter_login("standard_user")
+        self.login_page.enter_password("secret_sauce")
+        self.login_page.login_button()
+        time.sleep(2)
+        self.products_page.click_add_to_cart_backpack()
+        self.products_page.click_add_to_cart_fleece_jacket()
+        time.sleep(2)
